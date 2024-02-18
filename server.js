b13e7bfdc965da5728126ee9506b43f608d0a526
gahette <gahette@icloud.com>
2024-02-18 18:26:37 +0100

    intégration au démarrage du serveur et test de la connexion à la base de données

--- server.js
+++ server.js
@@ -3,6 +3,9 @@
 const express = require('express')
 const cors = require('cors')
 
+/*** Import de la connexion à la DB */
+let DB = require('./db.config')
+
 /****************/
 /*** Initialisation de l'API */
 const app = express()
@@ -18,9 +21,14 @@ app.get('/', (req, res) => res.send(`I'm online. All is OK !`))
 app.get('*', (req, res) => res.status(501).send(`What the hell are you doing !?!`))
 
 /****************/
-/*** Start serveur */
-app.listen(process.env.SERVER_PORT, () => {
-console.log(`This server is running on port ${process.env.SERVER_PORT}. Have fun !`)
-})
+/*** Start serveur avec test DB */
+DB.authenticate()
+    .then(() => console.log('Database connection OK'))
+    .then(() => {
+        app.listen(process.env.SERVER_PORT, () => {
+            console.log(`This server is running on port ${process.env.SERVER_PORT}. Have fun !`)
+        })
+    })
+    .catch(err => console.log('Database Error', err))
 
 
