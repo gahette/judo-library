698ac01ea21f285ae85143bfc51ceb2560b587ab
gahette <gahette@icloud.com>
2024-02-18 18:11:17 +0100

    Creation du fichier de configuration à la base de données db.config.js

---
+++ db.config.js
@@ -0,0 +1,19 @@
+/****************/
+/*** Import des modules nécessaires */
+const {Sequelize} = require('sequelize')
+
+/****************/
+let sequelize = new Sequelize(
+    process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: "mysql",
+        logging: false
+    }
+)
+
+/****************/
+/*** Synchronisation des modèles */
+// sequelize.sync()
+
+module.exports = sequelize
